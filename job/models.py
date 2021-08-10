33556e40bca865829a9d5d798581a49a07f1226c
Omar_Abdulaziz <amoor350om@hotmail.com>
2021-08-11 02:41:12 +0300

    add jobs views&url

--- job/models.py
+++ job/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
@@ -22,6 +23,13 @@ class Job(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE) # use single qutue because model after used
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return self.name
