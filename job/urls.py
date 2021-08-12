01da25941c73e2b9768051e71ab80cee6ead9e4f
Omar_Abdulaziz <amoor350om@hotmail.com>
2021-08-12 18:49:32 +0300

    frontend template

--- job/urls.py
+++ job/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.urls import include, path
 
 from . import views
 
+app_name = 'job'
+
 urlpatterns = [
     path('', views.job_list),
-    path('<int:id>', views.job_detail),
+    path('<int:job_id>', views.job_detail, name='job_detail'),
 ]
--- job/views.py
+++ job/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 
 def job_list(request):
-    pass
 
-def job_detail(request, id):
-    pass
+    job_list = Job.objects.all()
+    # must be a dict
+    # context = {'name to use in template': name here}
+    context = {'jobs': job_list}
+    return render(request, 'job/job_list.html', context)
+    
+
+def job_detail(request, job_id):
+    
+    job_detail = Job.objects.get(id=job_id) #.filter
+    context = {'job': job_detail}
+    return render(request, 'job/job_detail.html', context)
